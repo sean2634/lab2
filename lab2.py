e1e1bc8bc6c58ea9b3874389b989cdd4e57667e5
Sean <tansean.25@ichat.sp.edu.sg>
2026-04-29 11:45:26 +0800

    new

--- lab2.py
+++ lab2.py
@@ -1,1 +1,26 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to GAyNESS")
+import statistics
+def display_main_menu():
+    print ("Enter some numbers seperated by commas")
+
+def get_user_input():
+    x = input('Enter numbers: ')
+    numbers = [int(num) for num in x.split(",")]
+    return numbers
+
+def calc_avg_temp(numbers):
+    avgtemp = sum(numbers)/len(numbers)
+    return avgtemp
+
+def calc_min_max_temp(numbers):
+    return min(numbers), max(numbers)
+
+
+def main():
+    numbers = get_user_input()
+    avgtemp = calc_avg_temp(numbers)
+    minmax = calc_min_max_temp(numbers)
+    print (avgtemp)
+    print (minmax)
+    print (statistics.median(numbers))
+if __name__ == "__main__":
+    main()
